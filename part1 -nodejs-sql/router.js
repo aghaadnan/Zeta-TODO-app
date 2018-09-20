9a4a99c936bbc3a757d060fb2b64280299a0b26a
Mr Hanzala <Muhammadhanzala12@gmail.com>
2018-09-21 01:53:24 +0500

    Get Post Edit Data with Postegre SQL

--- part1 -nodejs-sql/router.js
+++ part1 -nodejs-sql/router.js
@@ -8,22 +8,21 @@ var db = new Sequelize("postgres://postgres:hanzala12@localhost:5432/Tasks");
 
 var tasks=db.define("Tasks",{
     title:{type:Sequelize.STRING},
-    discription:{type:Sequelize.STRING}
+    discription:{type:Sequelize.STRING},
+    done:{type:Sequelize.BOOLEAN,defaultValue:false}
 });
 
 
 tasks.sync();
 
 router.get('/todo/api/v1.0/tasks',function(req,res){
-    res.writeHead(200,{'Content-Type':'text/html'});
+    
 tasks.findAll().then(function(result){
     if(result){
         
-        result.forEach(function(data){
-            res.write("Id : "+ data.id +" title : "+ data.title +" discription: "+data.discription+"<br>");
-        });
+         res.status(200).send(result);
     }
-    res.end();
+    
 });
 
 });
@@ -34,32 +33,53 @@ router.get('/todo/api/v1.0/tasks/:taskid',function(req,res){
     tasks.findById(id).then(function(result){
         
         if(result){
-        res.write("Id :"+ id);
-        res .write("  title:"+ result.title);
-        res .write("  discription:     "+ result.discription);
-        res.end();}
+        res.status(200).send(result);}
         else{
-            res.send("No data found");
+            res.status(400).send("No data found");
         }
     });
 });
 
+//adding record of task in database
 router.post('/todo/api/v1.0/tasks',function(req,res){
-    var title=req.body.object.title;
+    var title=req.body.title;
     var discription= req.body.discription;
+    
     tasks.create({title:title,discription:discription});
     res.send("Titile : "+ title +"    Description :"+ discription);
     res.end("Request to tasks with Post method arrives");
 });
 
+//updating tasks against id
 router.put('/todo/api/v1.0/tasks/:taskid',function(req,res){
-
-res.end("request comes to update task of id : "+ req.params.taskid);
-
+var id = req.params.taskid;
+var title = req.body.title;
+var discription= req.body.discription;
+var done = req.body.done;
+console.log(req.body.title);
+tasks.findById(id).then(function(project){
+    project.updateAttributes({
+        title:title,
+        discription:discription,
+        done:done
+    });
 });
 
 
+});
+
+//deleting taks against given id
 router.delete("/todo/api/v1.0/tasks/:taskid",function(req,res){
-    res.end("Deleting id: "+ req.params.taskid);
+    console.log("Delete request arrives");
+    var id = req.params.taskid;
+    tasks.findById(id).then(function(project){
+        if(project){
+        project.destroy();
+        res.status(200).send({sucess:true});
+        }
+        else{
+            res.status(404).send({success:false});
+        }
+    })
 });
 module.exports=router;
