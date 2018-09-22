ce763ecc32f77fa65bae884758f62537de27537d
M Awais Akram <awaisakram64@gmail.com>
2018-09-22 17:51:36 +0500

    Create todo_client.py
    Client end to send request

---
+++ Part1/Step2 -gRPC API with NoSQL Databases/todo_client.py
@@ -0,0 +1,28 @@
+import todo_pb2, todo_pb2_grpc, grpc
+
+def list_task(stub):
+    result = stub.List(todo_pb2.Empty())
+    print(result)
+
+
+def get_task(stub):
+    
+    ''' Get function is define in todo_server.py file to get task by id'''
+    ID = todo_pb2.TodoId()
+    ID.id = '2'
+    tasks = stub.Get(bin(ID))
+    if not tasks:
+        print("NO task found against awd")
+        return
+    else:
+        print(tasks)
+    
+def run():
+    channel = grpc.insecure_channel('localhost:50051') 
+    stub = todo_pb2_grpc.TodosServiceStub(channel)
+    print("-------------- ListFeature --------------")
+    list_task(stub)
+    print("-------------- GetFeatures --------------")
+    get_task(stub)
+
+run()
