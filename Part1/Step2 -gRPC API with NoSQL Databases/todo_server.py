8e468645231fff51eaf5ba145d024a4ae0f20f04
M Awais Akram <awaisakram64@gmail.com>
2018-09-22 12:52:06 +0500

    Update todo_server.py

--- Part1/Step2 -gRPC API with NoSQL Databases/todo_server.py
+++ Part1/Step2 -gRPC API with NoSQL Databases/todo_server.py
@@ -10,9 +10,9 @@ import todo_pb2_grpc
 import todo_func
 from astroid.tests.testdata.python2.data.all import name
 
-def get_user(db,tskid):
+def get_task(db,tskid):
     for usr in db:
-        if tskid == usr['userid']:
+        if tskid == usr['taskid']:
             return usr
 
 
@@ -22,16 +22,36 @@ class TodoServicer(todo_pb2_grpc.TodosServiceServicer):
         self.db = todo_func.List_database()
     
     def List(self,request,context):
-        response = todo_pb2.TodoList
-        response.todo = db
+        response = self.db
+        if response is None or response == []:
+            return todo_pb2.Empty()
+        else:
+            return response
+        
+    def Get(self,request,context):
+        response = get_task(self.db, request)
         if response is None:
             return todo_pb2.Todo(id=request,title="")
         else:
             return response
         
-    def Get(self,request,context):
-        response = get_user(self.db, request)
-        if response is None:
-            return todo_pb2.Todo(id=request,title="")
-        else:
-            return response        
+# create a gRPC server
+server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+# use the generated function `add_TodoServicer_to_server`
+# to add the defined class to the server
+todo_pb2_grpc.add_TodosServiceServicer_to_server(        
+    TodoServicer(), server)
+
+# listen on port 50051
+print('Starting server. Listening on port 50051.')
+server.add_insecure_port('[::]:50051')
+server.start()
+
+# since server.start() will not block,
+# a sleep-loop is added to keep alive
+try:
+    while True:
+        time.sleep(86400)
+except KeyboardInterrupt:
+    server.stop(0)
